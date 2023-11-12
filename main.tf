519a6a046794da334c40a9d5cf73b7e0e2c9a68d
awscloudgirl <awscloudgirl@gmail.com>
2023-11-12 00:33:22 +0000

    reset to destroy

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
 terraform {
-  backend "remote" {
-    hostname = "app.terraform.io"
-    organization = "ExamPro"
+  #backend "remote" {
+  #  hostname = "app.terraform.io"
+  #  organization = "ExamPro"
 
-    workspaces {
-      name = "terra-house-1"
-    }
-  }
+  #  workspaces {
+  #    name = "terra-house-1"
+  #  }
+  #}
   cloud {
     organization = "projects-awscloudgirl"
     workspaces {
@@ -20,6 +20,6 @@ module "terrahouse_aws" {
   source = "./modules/terrahouse_aws"
   user_uuid = var.user_uuid
   bucket_name = var.bucket_name
-  index_html_filepath = var.index_html_filepath
-  error_html_filepath = var.error_html_filepath
+  #index_html_filepath = var.index_html_filepath
+  #error_html_filepath = var.error_html_filepath
 }
